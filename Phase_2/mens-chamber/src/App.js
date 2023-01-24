e0c8f20c6c699a2662aa2794d639a07f5d24b891
+John <+>
2023-01-24 10:00:14 +0300

    created components

---
+++ Phase_2/mens-chamber/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Services from './components/Services'; 
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/services" component={Services} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
